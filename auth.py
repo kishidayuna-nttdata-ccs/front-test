2cab2f1f16231f5efc7cdf0c5be7bdd935c14784
kishidayuna-nttdata-ccs <kishidayuna@nttdata-ccs.co.jp>
2025-11-13 17:02:31 +0900

    Update auth.py
    edit

--- auth.py
+++ auth.py
@@ -4,4 +4,5 @@ VALID_USERNAME = os.environ["VALID_USERNAME"]
 VALID_PASSWORD = os.environ["VALID_PASSWORD"]
 
 def check_credentials(username, password):
+    print(username, password)
     return username == VALID_USERNAME and password == VALID_PASSWORD
